8a8d39fc5efd441eeca873e9c7c1d7d31e2c1f42
Temirkhan <temirkhan@nasukhov.me>
2022-10-03 13:27:52 +0200

    Character interface extension

--- src/Character/CharacterInterface.php
+++ src/Character/CharacterInterface.php
@@ -5,8 +5,26 @@ declare(strict_types=1);
 namespace TemirkhanN\Venture\Character;
 
 use TemirkhanN\Venture\Battle\TargetInterface;
+use TemirkhanN\Venture\Item\ItemInterface;
+use TemirkhanN\Venture\Player\Inventory\Slot;
 
 interface CharacterInterface extends TargetInterface
 {
+    public function decreaseHealth(int $amount): void;
+    public function increaseHealth(int $amount): void;
+    /**
+     * @return iterable<Slot>
+     */
+    public function showInventory(): iterable;
+    /**
+     * @return iterable<Equipment\EquipmentItem>
+     */
+    public function equipment(): iterable;
 
+    public function equip(Equipment\EquipmentItem $item): void;
+    public function canEquip(ItemInterface $item): bool;
+
+    public function discardItem(Slot $slot): void;
+
+    public function useItem(Slot $fromSlot): void;
 }
