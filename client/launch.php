eb40224cbcaa9749be169ea9a70ef6204227c6d2
Temirkhan <temirkhan@nasukhov.me>
2023-07-27 07:57:30 +0200

    Misc configuration for docker and host machine env

--- client/launch.php
+++ client/launch.php
@@ -6,6 +6,11 @@ use TemirkhanN\Venture\Game\Launcher\WebLauncher;
 
 ini_set('memory_limit','10M');
 
+$host = '172.17.0.2';
+if (isset($argv[1])) {
+    $host = $argv[1];
+}
+
 echo 'Launching...' . PHP_EOL;
 
 require_once __DIR__ . '/../bootstrap.php';
@@ -15,6 +20,6 @@ $di = require __DIR__ .'/di.php';
 /** @var \TemirkhanN\Venture\Game\Launcher\WebLauncher $launcher */
 $launcher = $di->get(WebLauncher::class);
 
-$launcher->run('172.17.0.2:8080');
+$launcher->run($host . ':8080');
 
 echo 'Running...' . PHP_EOL;
